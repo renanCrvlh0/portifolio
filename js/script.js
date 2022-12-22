a619b5b9579feb486dd69c2f68c3fb576c1ed44b
Renan Carvalho <rfac2000@hotmail.com>
2022-12-21 21:22:45 -0300

    Adicionando funcionalidade de dropdown nas habilidades e alterando teste feito no css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 window.onload = () => {
+    const btnHabilidades = document.querySelectorAll('.btn-habilidades');
+    const teste = document.querySelectorAll('.container-botoes .teste');
+    const arrow = document.querySelectorAll('.btn-habilidades .arrow-symbol');
 
+    if (btnHabilidades.length && teste.length) {
+        teste[0].classList.add('ativo');
+
+        function ativar(index) {
+            teste.forEach((div) => {
+                div.classList.remove('ativo');
+            });
+            teste[index].classList.add('ativo');
+            
+        }
+
+        btnHabilidades.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                ativar(index);
+            });
+        });
+    }
 
 }
