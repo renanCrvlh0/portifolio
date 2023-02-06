9f13a65475f04837f66b428a07176981bd74bc5d
Renan Araujo <renan.araujo@corp.bs2.com.br>
2023-02-06 18:09:25 -0300

    Adicionando pop up de habilidades

--- js/ativar.js
+++ js/ativar.js
@@ -1,8 +1,40 @@
-export function ativar() {
-  const ativar = document.querySelector('.ativar');
+export function ativarFront() {
+  const frontEnd = document.querySelector('.front');
   const containerPrincipal = document.querySelector('.container-principal');
 
-  ativar.classList.add('habilidade');
+  frontEnd.classList.add('habilidade');
+  containerPrincipal.classList.add('ativar-opacidade');
+}
+
+export function ativarBack() {
+  const backEnd = document.querySelector('.back');
+  const containerPrincipal = document.querySelector('.container-principal');
+
+  backEnd.classList.add('habilidade');
+  containerPrincipal.classList.add('ativar-opacidade');
+}
+
+export function ativarBanco() {
+  const bancoDados = document.querySelector('.banco');
+  const containerPrincipal = document.querySelector('.container-principal');
+
+  bancoDados.classList.add('habilidade');
+  containerPrincipal.classList.add('ativar-opacidade');
+}
+
+export function ativarUi() {
+  const uiUx = document.querySelector('.design');
+  const containerPrincipal = document.querySelector('.container-principal');
+
+  uiUx.classList.add('habilidade');
+  containerPrincipal.classList.add('ativar-opacidade');
+}
+
+export function ativarFrame() {
+  const frameWork = document.querySelector('.frame');
+  const containerPrincipal = document.querySelector('.container-principal');
+
+  frameWork.classList.add('habilidade');
   containerPrincipal.classList.add('ativar-opacidade');
 }
 
--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 import { darkMode } from './darkMode.js';
-import { ativar } from './ativar.js';
+import { ativarFront } from './ativar.js';
+import { ativarBack } from './ativar.js';
+import { ativarBanco } from './ativar.js';
+import { ativarUi } from './ativar.js';
+import { ativarFrame } from './ativar.js';
 import { desativar } from './ativar.js';
 
-const fechar = document.querySelector('.fechar');
+const fechar = document.querySelectorAll('.fechar');
 const btnDarkMode = document.querySelector('.btn-darkLight');
 
-const front = document.querySelector('.front');
+const front = document.querySelector('.front-end');
+const back = document.querySelector('.back-end');
+const banco = document.querySelector('.banco-dados');
+const design = document.querySelector('.ui-ux');
+const frame = document.querySelector('.frameworks');
+const soft = document.querySelector('.soft-skills');
 
 btnDarkMode.addEventListener('click', darkMode);
 
-front.addEventListener('click', ativar);
+front.addEventListener('click', ativarFront);
+back.addEventListener('click', ativarBack);
+banco.addEventListener('click', ativarBanco);
+design.addEventListener('click', ativarUi);
+frame.addEventListener('click', ativarFrame);
 
-fechar.addEventListener('click', desativar);
+fechar.forEach((item) => {
+  item.addEventListener('click', desativar);
+});
