987b8fe75e0c0ebbb76e6ad41769c7dc8ae05a45
Renan Araujo <renan.araujo@corp.bs2.com.br>
2023-02-18 22:30:08 -0300

    Adicionando funcionalidade de mudança de cores no portifólio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { darkMode, yellowMode, blueMode } from './darkMode.js';
+import { darkMode } from './darkMode.js';
 import { ativarFront } from './ativar.js';
 import { ativarBack } from './ativar.js';
 import { ativarBanco } from './ativar.js';
@@ -10,6 +10,12 @@ const fechar = document.querySelectorAll('.fechar');
 const btnDarkMode = document.querySelector('.btn-darkLight');
 const btnYellow = document.querySelector('#amarelo');
 const btnBlue = document.querySelector('#azul');
+const btnPink = document.querySelector('#rosa');
+const btnPurple = document.querySelector('#roxo');
+const btnRed = document.querySelector('#vermelho');
+const btnGreen = document.querySelector('#verde');
+
+const btnCores = document.querySelector('.aba-cores');
 
 const front = document.querySelector('.front-end');
 const back = document.querySelector('.back-end');
@@ -19,8 +25,21 @@ const frame = document.querySelector('.frameworks');
 const soft = document.querySelector('.soft-skills');
 
 btnDarkMode.addEventListener('click', darkMode);
-btnYellow.addEventListener('click', yellowMode);
-btnBlue.addEventListener('click', blueMode);
+
+const cores = document.querySelectorAll('.cores div');
+const doc = document.documentElement;
+
+cores.forEach((item) => {
+  item.addEventListener('click', () => {
+    const cor = item.id;
+    doc.classList.add(cor);
+  });
+});
+
+btnCores.addEventListener('click', () => {
+  const containerCores = document.querySelector('.container-cores');
+  containerCores.classList.toggle('ativarCores');
+});
 
 front.addEventListener('click', ativarFront);
 back.addEventListener('click', ativarBack);
